f275b220eb59853c65785335bcaa830ef89d84c9
Astroboy072008 <112554577+Astroboy072008@users.noreply.github.com>
2026-02-24 01:11:04 -0600

    Added Copy Methods for Image, Pixel and ImageContainer

--- src/ImageContainer.java
+++ src/ImageContainer.java
@@ -25,4 +25,36 @@ public class ImageContainer
 
         return pixels;
     }
+
+    public BufferedImage imageCopy()
+    {
+        BufferedImage copy = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+
+        for (int i = 0; i < pixels.length; i++)
+        {
+            for (int j = 0; j < pixels[i].length; j++)
+            {
+                copy.setRGB(i, j, pixels[i][j].getIntARGB());
+            }
+        }
+
+        return copy;
+    }
+
+    public Pixel[][] pixelsCopy()
+    {
+        Pixel[][] copy = new Pixel[pixels.length][pixels[0].length];
+
+        for (int i = 0; i < copy.length; i++)
+        {
+            System.arraycopy(pixels[i], 0, copy[i], 0, copy[i].length);
+        }
+
+        return copy;
+    }
+
+    public ImageContainer copy()
+    {
+        return new ImageContainer(imageCopy());
+    }
 }
