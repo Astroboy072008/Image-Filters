10252e0582f3953038c8c74b0f25cd6b10ea8749
Astroboy072008 <112554577+Astroboy072008@users.noreply.github.com>
2026-02-24 01:10:12 -0600

    Added individual ARGB set Methods

--- src/Pixel.java
+++ src/Pixel.java
@@ -25,12 +25,17 @@ public class Pixel {
         b = argb & 0xff;
     }
 
+    public int getIntARGB()
+    {
+        return argb;
+    }
+
     public void setARGB(int a, int r, int g, int b)
     {
-        this.a = a & 0xff;
-        this.r = r & 0xff;
-        this.g = g & 0xff;
-        this.b = b & 0xff;
+        this.a = Math.max(0, Math.min(255, a));
+        this.r = Math.max(0, Math.min(255, r));
+        this.g = Math.max(0, Math.min(255, g));
+        this.b = Math.max(0, Math.min(255, b));
 
         argb = (this.a << 24) | (this.r << 16) | (this.g << 8) | this.b;
 
@@ -42,6 +47,26 @@ public class Pixel {
         this.image = image;
 
         image.setRGB(x, y, argb);
+    }
+
+    public void setA(int a)
+    {
+        setARGB(a, r, g, b);
+    }
+
+    public void setR(int r)
+    {
+        setARGB(a, r, g, b);
+    }
+
+    public void setG(int g)
+    {
+        setARGB(a, r, g, b);
+    }
+
+    public void setB(int b)
+    {
+        setARGB(a, r, g, b);
     }
 
     public double getLuminance()
